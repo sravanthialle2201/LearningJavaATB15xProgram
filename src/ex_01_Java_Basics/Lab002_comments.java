5517f481eaa42747db965b3d974af0ee967a14a9
sravanthi <sravanthialle22@gmail.com>
2025-12-21 21:57:33 +0530

    Adding my codes

---
+++ src/ex_01_Java_Basics/Lab002_comments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab002_comments {
+    public static void main(String[] args) {
+        //single line comments
+        /*
+         * multiple comments
+         * used mainly for documentation
+         */
+        System.out.println("comments");
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab003_No_Main_method.java
@@ -0,0 +1,5 @@
+package ex_01_Java_Basics;
+
+public class Lab003_No_Main_method {
+    //program with no main method
+}
