4c566f3f345960a7397bbd37ce34064c88776912
sravanthi <sravanthialle22@gmail.com>
2025-12-21 22:27:06 +0530

    Adding my codes

---
+++ src/ex_01_Java_Basics/Lab004_Keywords.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Keywords {
+    public static void main(String[] args) {
+        System.out.println("Keywords in this program");
+        //public
+        //class
+        //package
+        //static
+        //void
+
+
+
+    }
+}
+
